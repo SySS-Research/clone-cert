de1cd933d4d5c35405fdc979811939c5a3eaf687
Adrian Vollmer <adrian.vollmer@syss.de>
2019-03-10 17:44:09 +0100

    improve readability with hexlify and unhexlify

--- clone-cert.sh
+++ clone-cert.sh
@@ -4,23 +4,12 @@
 # https://security.stackexchange.com/questions/127095/manually-walking-through-the-signature-validation-of-a-certificate
 
 set -e
-set -u
-
-HOST="$1"
-COMPROMISED_CA="$2"
-COMPROMISED_KEY="$3"
-
-if [[ -f "$HOST" ]] ; then
-    FILENAME="$(basename "$HOST")"
-else
-    SERVER="$(printf "%s" "$HOST" | cut -f1 -d:)"
-fi
 
 DIR="/tmp/"
 KEYLENGTH=2048 # 2048 is faster, but less secure than 4096
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ "$HOST" = "" ] ; then
+if [ "$1" = "" ] ; then
 cat <<EOF
 Usage: $0 <host>:<port>|<pem-file> [<subject> <key>]
 
@@ -31,10 +20,26 @@ As optional parameters, you can specifiy the distinguished name of the
 subject of a certificate and the corresponding private key in PEM format.
 This script will clone all certificates in the chain below the compromised
 one.
+
+If none of the certificates in the chain have a subject name that matches
+the one of the certificate that you control, the subject of the cloned host
+certificate will be changed accordingly and it is assumed that your
+certificate is a trust anchor.
 EOF
     exit 1
 fi
 
+HOST="$1"
+COMPROMISED_CA="$2"
+COMPROMISED_KEY="$3"
+
+set -u
+
+if [[ -f "$HOST" ]] ; then
+    FILENAME="$(basename "$HOST")"
+else
+    SERVER="$(printf "%s" "$HOST" | cut -f1 -d:)"
+fi
 
 function oid() {
     # https://bugzil.la/1064636
@@ -65,6 +70,16 @@ function oid() {
     esac
 }
 
+
+function hexlify(){
+    xxd -p -c99999
+}
+
+function unhexlify(){
+    xxd -p -r
+}
+
+
 if [[ -f "$HOST" ]] ; then
     CLONED_CERT_FILE="$DIR$FILENAME.cert"
     CLONED_KEY_FILE="$DIR$FILENAME.key"
@@ -88,20 +103,19 @@ OLD_MODULUS="$(openssl x509 -in "$ORIG_CERT_FILE" -modulus -noout \
 KEY_LEN="$(openssl x509  -in "$ORIG_CERT_FILE" -noout -text \
     | grep Public-Key: | grep -o "[0-9]\+")"
 
-
 MY_PRIV_KEY="$DIR$HOST.$KEY_LEN.key"
 MY_PUBL_KEY="$DIR$HOST.$KEY_LEN.cert"
 
 offset="$(openssl asn1parse -in "$ORIG_CERT_FILE" | grep SEQUENCE \
     | tail -n1 |sed 's/ \+\([0-9]\+\):.*/\1/' | head -n1)"
 SIGNING_ALGO="$(openssl asn1parse -in "$ORIG_CERT_FILE" \
-    -strparse $offset -noout -out >(xxd -p -c99999))"
+    -strparse $offset -noout -out >(hexlify))"
 offset="$(openssl asn1parse -in "$ORIG_CERT_FILE" \
     | tail -n1 |sed 's/ \+\([0-9]\+\):.*/\1/' | head -n1)"
 OLD_SIGNATURE="$(openssl asn1parse -in "$ORIG_CERT_FILE" \
-    -strparse $offset -noout -out >(xxd -p -c999999))"
+    -strparse $offset -noout -out >(hexlify))"
 OLD_TBS_CERTIFICATE="$(openssl asn1parse -in "$ORIG_CERT_FILE" \
-    -strparse 4 -noout -out >(xxd -p -c99999))"
+    -strparse 4 -noout -out >(hexlify))"
 
 # TODO support DSA, EC
 openssl req -new -newkey rsa:$KEY_LEN -days 356 -nodes -x509 \
@@ -114,12 +128,12 @@ NEW_TBS_CERTIFICATE="$(printf "%s" "$OLD_TBS_CERTIFICATE" \
     | sed "s/$OLD_MODULUS/$NEW_MODULUS/")"
 
 digest="$(oid "$SIGNING_ALGO")"
-NEW_SIGNATURE="$(printf "%s" "$NEW_TBS_CERTIFICATE" | xxd -p -r | \
-    openssl dgst -$digest -sign "$MY_PRIV_KEY" | xxd -p -c99999)"
+NEW_SIGNATURE="$(printf "%s" "$NEW_TBS_CERTIFICATE" | unhexlify | \
+    openssl dgst -$digest -sign "$MY_PRIV_KEY" | hexlify)"
 
-openssl x509 -in "$ORIG_CERT_FILE" -outform DER | xxd -p -c99999 \
+openssl x509 -in "$ORIG_CERT_FILE" -outform DER | hexlify \
     | sed "s/$OLD_MODULUS/$NEW_MODULUS/" \
-    | sed "s/$OLD_SIGNATURE/$NEW_SIGNATURE/" | xxd -r -p \
+    | sed "s/$OLD_SIGNATURE/$NEW_SIGNATURE/" | unhexlify \
     | openssl x509 -inform DER -outform PEM > "$CLONED_CERT_FILE"
 
 cp "$MY_PRIV_KEY" "$CLONED_KEY_FILE"
