e128c8685781baef2aab11ae25e299e690306b23
Adrian Vollmer <adrian.vollmer@syss.de>
2019-06-24 21:56:13 +0200

    add fake ca case to test script

--- tests/run.sh
+++ tests/run.sh
@@ -11,4 +11,8 @@ for i in `cat $SCRIPT_PATH/cases` ; do
     clone-cert -d="$SCRIPT_PATH/cache" -r $i > /dev/null || echo $i failed
 done
 
+clone-cert -d="$SCRIPT_PATH/cache" -r \
+    -c="$SCRIPT_PATH/test-ca.crt" -k="$SCRIPT_PATH/test-ca.key" \
+    www.syss.de:443 > /dev/null || echo Fake CA failed
+
 # rm -rf "$SCRIPT_PATH/cache"
